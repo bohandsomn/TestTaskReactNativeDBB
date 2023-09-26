2450d328e702e95708ce12f7e0be315bdc17a9a9
Bohdan Lukianchenko <bohdan.lukianchenko@gmail.com>
2023-09-26 16:28:59 +0300

    Create navigation

---
+++ src/shared/navigation/index.ts
@@ -0,0 +1,4 @@
+export { NavigationProvider } from './providers'
+export { useAppNavigation, useAppPageNavigation } from './lib'
+export { Navigator, AppLink } from './ui'
+export { PageNames } from './const'
---
+++ src/shared/navigation/lib/index.ts
@@ -0,0 +1,4 @@
+export * from './useNavigation'
+export * from './navigation'
+export * from './pageNavigation'
+export * from './usePageNavigation'
